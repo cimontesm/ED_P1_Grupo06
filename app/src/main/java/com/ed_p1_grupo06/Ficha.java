8a83fd69f46d37898332fff323dd3085a09c02fd
cimontesm <cimontes@espol.edu.ec>
2025-01-08 18:52:04 -0500

    Clase ficha

--- app/src/main/java/com/ed_p1_grupo06/Ficha.java
+++ app/src/main/java/com/ed_p1_grupo06/Ficha.java
@@ -2,4 +2,19 @@ package com.ed_p1_grupo06;
 
 public class Ficha {
     //en vez de tener la clase jugador y dividirla en 2 con la ficha se puede manejar los movimientos
+    public enum TipoFicha { CRUZ, CIRCULO, VACIO }
+
+    private TipoFicha tipo;
+
+    public Ficha(TipoFicha tipo) {
+        this.tipo = tipo;
+    }
+
+    public TipoFicha getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoFicha tipo) {
+        this.tipo = tipo;
+    }
 }
