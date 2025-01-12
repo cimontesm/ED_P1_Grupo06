c165c6b80fee86e89f4da4e7f8c7a7cf2f1f0fa0
vicmmoraM <vicmmora@espol.edu.ec>
2025-01-12 01:36:17 -0500

    Creacion de layout

--- app/src/main/java/com/ed_p1_grupo06/MainActivity.java
+++ app/src/main/java/com/ed_p1_grupo06/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ed_p1_grupo06;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +13,9 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button salir;
     Button jugar;
+    Button jvb;
+    Button jvj;
+    Button salir1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +27,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        jugar = (Button) findViewById(R.id.ID_button);
-        salir = (Button) findViewById(R.id.button3);
+        jugar = (Button) findViewById(R.id.ButtonJugar);
+        salir = (Button) findViewById(R.id.ButtonSalir);
 
         jugar.setOnClickListener(v ->{
             setContentView(R.layout.menu_seleccion);
+            jvb = findViewById(R.id.JGB);
+            jvj = findViewById(R.id.JGJG);
+            salir1 = findViewById(R.id.button4);
+        });
+
+        jvb.setOnClickListener(v ->{
+
+        });
+
+        salir1.setOnClickListener(v ->{
+            finishAffinity();
         });
 
         salir.setOnClickListener(v ->{
             finishAffinity();
         });
+
     }
 }
