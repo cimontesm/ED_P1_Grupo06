7da8c9283d2b21f74ddc544ac5a7adbc1f52b918
vicmmoraM <vicmmora@espol.edu.ec>
2025-01-13 21:19:42 -0500

    Creacion de menus y salas

--- app/src/main/java/com/ed_p1_grupo06/MainActivity.java
+++ app/src/main/java/com/ed_p1_grupo06/MainActivity.java
@@ -1,6 +1,5 @@
 package com.ed_p1_grupo06;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -11,26 +10,47 @@ import androidx.core.view.WindowInsetsCompat;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Button jugar;
-Button salir;
+Button jugar, salir, jugvsbot, jugvsjug, botvsbot,volver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu_principal);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        //Seccion menu_principal
         jugar = (Button) findViewById(R.id.ButtonJugar);
         salir = (Button) findViewById(R.id.ButtonSalir);
         jugar.setOnClickListener(v ->{
-            setContentView(R.layout.menu_principal);
+            //Seccion menu_seleccion
+            setContentView(R.layout.menu_seleccion);
+            jugvsbot = (Button) findViewById(R.id.escenario1);
+            jugvsjug = (Button) findViewById(R.id.escenario2);
+            botvsbot = (Button) findViewById(R.id.escenario3);
+            volver = (Button) findViewById(R.id.escenario4);
+            //jugvsbot
+            jugvsbot.setOnClickListener(s ->{
+                setContentView(R.layout.menu_escogerjugvsbot);
+            });
+            //jugvsjug
+            jugvsjug.setOnClickListener(s ->{
+                setContentView(R.layout.menu_escogerjugvsjug);
+            });
+            //botvsbot
+            botvsbot.setOnClickListener(s ->{
+                setContentView(R.layout.menu_escogerbotvsbot);
+            });
+            //volver
+            volver.setOnClickListener(s ->{
+                setContentView(R.layout.menu_principal);
+            });
         });
         salir.setOnClickListener(v ->{
             finishAffinity();
         });
+
     }
 }
