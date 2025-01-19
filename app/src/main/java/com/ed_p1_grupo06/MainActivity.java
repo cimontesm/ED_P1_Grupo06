778f25bdfbc7f56a21cc3eb928996d4550bcc76e
Jose Adrian <jadrian@espol.edu.ec>
2025-01-19 18:07:31 -0500

    cambios de los menús

--- app/src/main/java/com/ed_p1_grupo06/MainActivity.java
+++ app/src/main/java/com/ed_p1_grupo06/MainActivity.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button jugar, salir, jugvsbot, jugvsjug, botvsbot,volver,salajugvsbot,regresoSelc, salajugvsjug, regresoSelc2, salabotvsbot, regresoSelec3;
@@ -33,15 +31,9 @@ public class MainActivity extends AppCompatActivity {
         //Seccion menu_principal
         jugar = (Button) findViewById(R.id.ButtonJugar);
         salir = (Button) findViewById(R.id.ButtonSalir);
-        jugar.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe));
-        salir.setBackgroundTintList(ContextCompat.getColorStateList(this,R.color.cafe));
-
 
         jugar.setOnClickListener(v -> cargarMenuSeleccion());
-
         salir.setOnClickListener(v -> finishAffinity());
-
-
     }
 
     //Pantalla de carga
@@ -52,22 +44,17 @@ public class MainActivity extends AppCompatActivity {
         botvsbot = (Button) findViewById(R.id.escenario3);
         volver = (Button) findViewById(R.id.escenario4);
 
-        jugvsbot.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe_claro));
-        jugvsjug.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe_claro));
-        botvsbot.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe_claro));
-        volver.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe_oscuro));
-
         jugvsbot.setOnClickListener(s -> {
             modoDeJuego = "Jugador vs Bot";
-            cargarMenuEscogerJugVsBot();
+            cargarMenuEscoger();
         });
         jugvsjug.setOnClickListener(s -> {
             modoDeJuego = "Jugador vs Jugador";
-            cargarMenuEscogerJugvsJug();
+            cargarMenuEscoger();
         });
         botvsbot.setOnClickListener(s -> {
             modoDeJuego = "Bot vs Bot";
-            cargarMenuEscogerBotvsBot();
+            cargarMenuEscoger();
         });
         volver.setOnClickListener(s -> cargarMenuPrincipal());
     }
@@ -78,51 +65,20 @@ public class MainActivity extends AppCompatActivity {
         jugar = (Button) findViewById(R.id.ButtonJugar);
         salir = (Button) findViewById(R.id.ButtonSalir);
 
-        jugar.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe));
-        salir.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe));
-
         // Reasignar listeners
         jugar.setOnClickListener(v -> cargarMenuSeleccion());
         salir.setOnClickListener(v -> finishAffinity());
     }
 
     //Jugador vs Bot
-    private void cargarMenuEscogerJugVsBot() {
-        setContentView(R.layout.menu_escogerjugvsbot);
+    private void cargarMenuEscoger() {
+        setContentView(R.layout.menu_escoger);
         salajugvsbot = (Button) findViewById(R.id.IngresoSala3);
         regresoSelc = (Button) findViewById(R.id.RegresoSeleccion3);
 
-        salajugvsbot.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe));
-        regresoSelc.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe_oscuro));
         salajugvsbot.setOnClickListener(v -> cargarTablero());
         regresoSelc.setOnClickListener(v -> cargarMenuSeleccion());
 
-    }
-
-
-    private void cargarMenuEscogerJugvsJug(){
-        setContentView(R.layout.menu_escogerjugvsjug);
-        salajugvsjug = (Button) findViewById(R.id.IngresoSala2);
-        regresoSelc2 = (Button) findViewById(R.id.RegresoSeleccion2);
-
-        salajugvsjug.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe));
-        regresoSelc2.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe_oscuro));
-
-        regresoSelc2.setOnClickListener(v -> cargarMenuSeleccion());
-        salajugvsjug.setOnClickListener(v -> cargarTablero());
-
-    }
-
-    private void cargarMenuEscogerBotvsBot(){
-        setContentView(R.layout.menu_escogerbotvsbot);
-        salabotvsbot = (Button) findViewById(R.id.IngresoSala3);
-        regresoSelec3 = (Button) findViewById(R.id.RegresoSeleccion3);
-
-        salabotvsbot.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe));
-        regresoSelec3.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cafe_oscuro));
-
-        regresoSelec3.setOnClickListener(v -> cargarMenuSeleccion());
-        salabotvsbot.setOnClickListener(v -> cargarTablero());
     }
 
     //Cargar Tablero
